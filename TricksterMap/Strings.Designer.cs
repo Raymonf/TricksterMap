7bb32eeeba9e7e280cca53bc513f1cb55a3f5cee
Raymonf <raymonf@outlook.com>
2022-12-31 15:38:54 -0500

    Update Chinese "open file" string

--- TricksterMap/Strings.Designer.cs
+++ TricksterMap/Strings.Designer.cs
@@ -19,7 +19,7 @@ namespace TricksterMap {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Strings {
@@ -403,7 +403,7 @@ namespace TricksterMap {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Set Debris (?).
+        ///   Looks up a localized string similar to Check Level Requirement.
         /// </summary>
         internal static string SetDebris {
             get {
